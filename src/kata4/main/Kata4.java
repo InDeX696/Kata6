3c900c6cc8d9b4f368823e728b2d9e0041bc464f
usuario <usuario@10.22.148.62>
2017-11-03 10:12:45 +0000

    Kata4 Version 2

--- src/kata4/main/Kata4.java
+++ src/kata4/main/Kata4.java
@@ -18,16 +18,37 @@ import kata4.view.MailListReader;
  * @author usuario
  */
 public class Kata4 {
+   
 
     /**
      * @param args the command line arguments
      */
+    private List<Mail> mailList;
+    private Histogram<String> histogram;
+    
     public static void main(String[] args) throws FileNotFoundException, IOException {
         // TODO code application logic here
+        Kata4 kata4 =  new Kata4();
+        kata4.execute();
+ 
+    }
+    
+    public void execute() throws FileNotFoundException, IOException{
+        input();
+        process();
+        output();
+    }
+    private void input() throws FileNotFoundException, IOException{
         String filename = "C:\\Users\\usuario\\Desktop\\emails.txt";
-        List<Mail> mailList = MailListReader.read(filename);
-        Histogram<String> histogram = MailHistogramBuilder.build(mailList);
-        HistogramDisplay histoDisplay = new HistogramDisplay(histogram);
+        mailList = MailListReader.read(filename);  
+    }
+    
+    private  void process(){
+       
+        histogram = MailHistogramBuilder.build(mailList);
+    }
+    private void output(){
+         HistogramDisplay histoDisplay = new HistogramDisplay(histogram);
         histoDisplay.execute();
     }
     
